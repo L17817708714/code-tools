d632fe2c37d52506949d3259e02829595adadf92
17817708714 <L17817708714@163.com>
2021-06-19 14:02:57 +0800

    init

---
+++ mybatis-generator/src/main/java/com/MyCommentCenerator.java
@@ -0,0 +1,23 @@
+package com;
+
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.api.dom.java.Method;
+import org.mybatis.generator.internal.DefaultCommentGenerator;
+
+public class MyCommentCenerator extends DefaultCommentGenerator {
+
+    @Override
+    public void addFieldComment(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+        String remarks = introspectedColumn.getRemarks();
+        field.addJavaDocLine("/**");
+        field.addJavaDocLine(" * " + remarks);
+        field.addJavaDocLine(" */");
+    }
+
+    @Override
+    public void addGetterComment(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+        return;
+    }
+}
---
+++ mybatis-generator/src/main/java/example/mapper/TUserMapper.java
@@ -0,0 +1,16 @@
+package example.mapper;
+
+import example.model.TUser;
+import java.util.List;
+
+public interface TUserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(TUser record);
+
+    TUser selectByPrimaryKey(Integer id);
+
+    List<TUser> selectAll();
+
+    int updateByPrimaryKey(TUser record);
+}
