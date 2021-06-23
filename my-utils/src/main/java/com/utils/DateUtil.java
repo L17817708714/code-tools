fd4b71a768f9433b9352581bbd3ff01cc71b04a3
17817708714 <L17817708714@163.com>
2021-06-23 22:54:00 +0800

    工具类完善

--- my-utils/src/main/java/com/utils/DateUtil.java
+++ my-utils/src/main/java/com/utils/DateUtil.java
@@ -17,6 +17,7 @@ public class DateUtil {
 
     public static SimpleDateFormat dateFormatAsHours = new SimpleDateFormat("HH");
 
+    public static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     /**
      * @Description: 获取 年-月-日，例如："1996-08-07"
      * @Author: luozhihong
@@ -25,17 +26,6 @@ public class DateUtil {
      **/
     public static String converToStringAsDay(Date date) {
         return dateFormatAsDay.format(date);
-    }
-
-    /**
-     * @Description: 字符串转换为 Date，
-     * @Author: luozhihong
-     * @Date: 2021/6/22
-     * @param param: 参数类型："1996-08-07"
-     * @return: java.util.Date
-     */
-    public static Date converToDateAsDay(String param) throws ParseException {
-        return dateFormatAsDay.parse(param);
     }
 
     /**
@@ -50,16 +40,38 @@ public class DateUtil {
     }
 
     /**
-     * @Description: 获取 5 分钟前的时间
+     * @Description: 字符串转换为 Date，
+     * @Author: luozhihong
+     * @Date: 2021/6/22
+     * @param param: 参数类型："1996-08-07"
+     * @return: java.util.Date
+     */
+    public static Date converToDateAsDay(String param) throws ParseException {
+        return dateFormatAsDay.parse(param);
+    }
+
+    /**
+     * @Description: 字符串转换为 Date
+     * @Author: luozhihong
+     * @Date: 2021/6/23
+     * @param param: 参数类型："1996-08-07 12:10:01"
+     * @return: java.util.Date
+     */
+    public static Date converToDate(String param) throws ParseException {
+        return dateFormat.parse(param);
+    }
+
+    /**
+     * @Description: 获取 amount 分钟前后的时间
      * @Author: luozhihong
      * @Date: 2021/6/22
      * @param date:
      * @return: java.util.Date
      **/
-    public static Date getDateBeforeFiveMinutes(Date date) {
+    public static Date getDateDiffAmountMinutes(Date date, int amount) {
         Calendar cal = Calendar.getInstance();
         cal.setTime(date);
-        cal.add(Calendar.MINUTE, -5);
+        cal.add(Calendar.MINUTE, amount);
         return cal.getTime();
     }
 
@@ -76,7 +88,7 @@ public class DateUtil {
         // 获得当前日期是一个星期的第几天
         int dayWeek = cal.get(Calendar.DAY_OF_WEEK);
         if (1 == dayWeek) {
-            cal.add(Calendar.DAY_OF_MONTH, -1);
+            cal.add(Calendar.DATE, -1);
         }
         // 获得当前日期是一个星期的第几天
         int day = cal.get(Calendar.DAY_OF_WEEK);
@@ -85,7 +97,7 @@ public class DateUtil {
     }
 
     /**
-     * @Description: 获取当前日期周末的时间
+     * @Description: 获取当前日期 周末的时间
      * @Author: luozhihong
      * @Date: 2021/6/22
      * @param date:
@@ -135,11 +147,42 @@ public class DateUtil {
         return cal.getTime();
     }
 
+    /**
+     * @Description: 获取 amount 天前后的时间
+     * @Author: luozhihong
+     * @Date: 2021/6/23
+     * @param date:
+     * @return: java.util.Date
+     */
+    public static Date getDateDiffAmountDay(Date date, int amount) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        // 当前日期 -1 天
+        cal.add(Calendar.DATE, amount);
+        return cal.getTime();
+    }
+
+    /**
+     * @Description: 获取当前日期，前一个小时时间
+     * @Author: luozhihong
+     * @Date: 2021/6/23
+     * @param date:
+     * @return: java.util.Date
+     */
+    public static Date getDiffAmountHour(Date date, int amount) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        // 当前时间 -1 小时
+        cal.add(Calendar.HOUR, amount);
+        return cal.getTime();
+    }
+
     public static void main(String[] args) throws ParseException {
-        SimpleDateFormat dateFormatAsDay = new SimpleDateFormat("yyyy-MM-dd");
-        String par = "2020-12-2";
+        SimpleDateFormat dateFormatAsDay = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String par = "2021-07-01 22:40:05";
         Date date = dateFormatAsDay.parse(par);
-        System.out.println(converToStringAsDay(getBeginMonth(date)));
-        System.out.println(converToStringAsDay(getEndMonth(date)));
+        System.out.println(dateFormatAsDay.format(getDateDiffAmountDay(date, -1)));
+        System.out.println(dateFormatAsDay.format(getDiffAmountHour(date, -1)));
+        System.out.println(dateFormatAsDay.format(getMonday(date)));
     }
 }
