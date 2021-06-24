6c60aeeae37b9ea6ef83ec315850aed4a7e9addb
17817708714 <L17817708714@163.com>
2021-06-24 23:57:06 +0800

    配置完善

--- mybatis-generator/src/main/java/com/MyCommentCenerator.java
+++ mybatis-generator/src/main/java/com/MyCommentCenerator.java
@@ -3,7 +3,6 @@ package com;
 import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.IntrospectedTable;
 import org.mybatis.generator.api.dom.java.Field;
-import org.mybatis.generator.api.dom.java.Method;
 import org.mybatis.generator.internal.DefaultCommentGenerator;
 
 public class MyCommentCenerator extends DefaultCommentGenerator {
@@ -14,10 +13,5 @@ public class MyCommentCenerator extends DefaultCommentGenerator {
         field.addJavaDocLine("/**");
         field.addJavaDocLine(" * " + remarks);
         field.addJavaDocLine(" */");
-    }
-
-    @Override
-    public void addGetterComment(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
-        return;
     }
 }
